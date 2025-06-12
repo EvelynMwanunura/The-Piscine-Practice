5bc2224ce8d0d4ff9360758555706f33ca5befe1
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-06-12 19:44:41 +0200

    Edited

--- Project-Spaced-Repetition-Tracker/getData.test.mjs
+++ Project-Spaced-Repetition-Tracker/getData.test.mjs
@@ -24,28 +24,3 @@ assert.deepStrictEqual(
   "Data does not match expected output"
 );
 console.log("getData: PASSED");
-
-/*test(
-  "getData returns correct data for user",
-  () => {
-    const userId = "user1";
-    const expectedData = [
-      { userId: "user1", topic: "Math", date: "2023-10-01" },
-      { userId: "user1", topic: "Science", date: "2023-10-02" },
-    ];
-
-    globalThis.localStorage = {
-      getItem: (key) => {
-        if (key === `stored-data-user-${userId}`) {
-          return JSON.stringify(expectedData);
-        }
-        return null;
-      },
-    };
-
-    const data = getData(userId);
-    assert.deepEqual(data, expectedData);
-  },
-  "Data should match expected data for user1"
-);
-*/
