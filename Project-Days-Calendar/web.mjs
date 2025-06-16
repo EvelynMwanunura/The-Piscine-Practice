ab4ed891be7399811d84004c14af87ff3d93eb57
shaden-pr <pratiamalden@gmail.com>
2025-06-16 18:22:00 +0100

    display calender

--- Project-Days-Calendar/web.mjs
+++ Project-Days-Calendar/web.mjs
@@ -3,9 +3,74 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import { getGreeting } from "./common.mjs";
-import daysData from "./days.json" with { type: "json" };
+//import { getGreeting } from "./common.mjs";
+//import daysData from "./days.json" with { type: "json" };
+let nav = 0;
 
-window.onload = function() {
-    document.querySelector("body").innerText = `${getGreeting()} - there are ${daysData.length} known days`;
+const yearSelect = document.getElementById("year-select")
+const monthSelect = document.getElementById("month-select")
+
+const calender = document.getElementById("calendar");
+const monthDisplay = document.getElementById("monthDisplay");
+
+const monthNames = [
+    'January','February','March','April','May','June',
+    'July','August','September','October','November','December'
+];
+const weekdays = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+function load(){
+    const today = new Date();
+
+    const day = today.getDate();
+    const month = today.getMonth();
+    const year = today.getFullYear();
+
+    const firstDay = new Date(year, month, 1); // first day of this month
+    const daysInMonth = new Date(year, month + 1, 0).getDate(); // 0 = last day of prev month
+
+    const dateString = firstDay.toLocaleDateString('en-GB', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric'
+    })
+
+    const paddingDays = weekdays.indexOf(dateString.split(', ')[0]); // if its sunday = 6
+
+    monthDisplay.innerText = `${today.toLocaleDateString("en-GB", {
+      month: "long",
+    })} ${year}`;
+
+
+    for (let i = 1; i <= paddingDays + daysInMonth; i++) {
+      const daySquare = document.createElement("div");
+      daySquare.classList.add("day");
+
+      if (i > paddingDays) {
+        daySquare.innerText = i - paddingDays;
+
+        daySquare.addEventListener("click", () => console.log("click"));
+      } else {
+        daySquare.classList.add("padding");
+      }
+      calender.appendChild(daySquare);
+    }
 }
+
+
+load();
+
+
+
+
+
+
