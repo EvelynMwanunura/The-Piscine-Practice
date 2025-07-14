71d8d66138d7a996cfbc53c3902e95be35608f50
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-07-14 16:12:42 +0200

    Implemented a function that returns an array of objects of songs that a selected user has listened to

--- Project-Music-Data/common.mjs
+++ Project-Music-Data/common.mjs
@@ -1,3 +1,14 @@
 import { getUserIDs } from "./data.mjs";
+import { getSong, getListenEvents } from "./data.mjs";
 
-export const countUsers = () => getUserIDs().length;
+export const renderSongs = (userID) => {
+  const songsUserListenedTo = getListenEvents(userID);
+  let listenedSongs = [];
+  songsUserListenedTo.forEach((song) => {
+    listenedSongs.push(getSong(song.song_id));
+  });
+
+  return listenedSongs;
+};
+
+console.log("songs rendered", renderSongs(3));
