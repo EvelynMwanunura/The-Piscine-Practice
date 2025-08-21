664f6366c32e33062b8bcaa45b99865183f0f68c
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-08-21 15:41:52 +0200

    Implemented a to show a calendar

--- Project-Days-Calendar/common.mjs
+++ Project-Days-Calendar/common.mjs
@@ -1,5 +1,41 @@
 // This is a placeholder file which shows how you can define functions which can be used from both a browser script and a node script. You can delete the contents of the file once you have understood how it works.
 
 export function getGreeting() {
-    return "Hello";
+  return "Hello";
+}
+
+export function getTheNthOccurrence(occurrence, weekday, month) {
+  let occurrenceMap = ["first", "second", "third", "fourth"];
+
+  const monthMap = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const weekDayMap = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  const date = new Date();
+  month = date.getMonth();
+  const year = date.getFullYear();
+
+  const firstDateOfMonth = new Date(month, year, 1);
+  console.log(firstDateOfMonth);
+  const firstDayOfMonth = firstDateOfMonth.getDay();
 }
--- Project-Days-Calendar/web.mjs
+++ Project-Days-Calendar/web.mjs
@@ -6,6 +6,64 @@
 import { getGreeting } from "./common.mjs";
 import daysData from "./days.json" with { type: "json" };
 
-window.onload = function() {
+/*window.onload = function() {
     document.querySelector("body").innerText = `${getGreeting()} - there are ${daysData.length} known days`;
+}*/
+
+const calendarElement = document.getElementById("calendar");
+
+const showCalendar = () => {
+const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",  "Sunday"]
+const table = document.createElement("table");
+const tableHead = document.createElement("thead")
+const tableRow = document.createElement("tr")
+
+for(let i = 0; i < weekdays.length; i++){
+    const tableRowContents = document.createElement("th")
+    tableRowContents.textContent = weekdays[i]
+    tableRow.appendChild(tableRowContents)
 }
+
+tableHead.appendChild(tableRow)
+table.appendChild(tableHead)
+calendarElement.appendChild(table)
+
+const currentDate = new Date();
+console.log(currentDate)
+const month = currentDate.getMonth()
+console.log(month)
+const year = currentDate.getFullYear()
+console.log(year)
+const date = currentDate.getDate()
+console.log(date)
+const firstDateOfMonth = new Date((month, year, 1))
+const firstDayOfMonth = firstDateOfMonth.getDay()
+console.log(firstDayOfMonth)
+const lastDateOfMonth = new Date( year, month + 1, 0)
+
+console.log(lastDateOfMonth.getDate())
+
+const tableBody = document.createElement("tbody")
+const row = tableBody.insertRow()
+for(let i = 0; i < firstDayOfMonth; i ++ ){
+   row.insertCell().textContent = "*"
+
+   for(let j = 0; j < lastDateOfMonth; i++){
+    if(row.cells.length === 7){
+        tableBody.insertRow()
+    }
+    row.insertCell().textContent = i;
+
+
+   }
+   table.appendChild(tableBody);
+    calendarElement.appendChild(table);
+}
+
+
+
+    
+}
+
+
+window.onload = showCalendar
